d5edcf03bdf0863d17d6b353880bd1e39b2f390a
rapidemporium <150315341+rapidemporium@users.noreply.github.com>
2023-11-14 10:33:20 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,22 @@ const CryptoJS = require('crypto-js');
 require('dotenv').config();
 const http = require('http');
 
+ 
 const partnerId = process.env.YOUR_PARTNER_ID;
 const secretKey = process.env.YOUR_SECRET_KEY;
 
-//setting up users Auth
-const { findOne } = require('./users');
+//other page routes --->
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home');
+});
 
-/* LOCAL PASSPORT STRATEGY */
+router.get('/mlbb-moogold', function(req, res, next){
+  res.render('index');
+})
 
+
+//creating order
 router.post('/proceed', function(req, res, next){
   //Obtain Product ID from Product Detail API
 const payload = {
@@ -52,71 +60,6 @@ axios.post("https://moogold.com/wp-json/v1/api/order/create_order", payload, {
 
 })
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('userAuth');
-});
-
-router.get('/mlbb-moogold', function(req, res, next){
-  res.render('index');
-})
-
-router.get('/home', async function(req, res, next){
-  let user = await userModel.findOne({email: req.session.passport.user});
-  res.render('home', {user});
-})
-
-router.get('/test', function(req, res, next){
-  res.render('test');
-})
 
 
-// router.use("/payment", async (req, res, next) => {
-
-// try{
-  
-//   var instance = new Razorpay({ key_id: process.env.KEY_ID, key_secret: process.env.KEY_SECRET })
- 
-//   var order = await instance.orders.create({
-//     amount: req.body.amount,  // amount in the smallest currency unit
-//     currency: "INR",
-//     receipt: "order_rcptid_11", 
-//   });
-
-// res.status(201).json({
-//   success: true,
-//   order,
-// });
-
-// }catch(err){
-//   next(err);
-// }
-
-// });
-
-
-
-// router.post('/pay-verify', async (req,res, next) => {
-
-//   var expectedSignature = crypto.createHmac('sha256', 'NbuBBPdNMiBejAITkjNeVHds')
-//                                   .update(req.body.razorpay_order_id + "|" + req.body.razorpay_payment_id)
-//                                   .digest('hex');
-//                                   console.log("sig"+req.body.razorpay_signature);
-//                                   console.log("sig"+expectedSignature);
- 
-//   if(expectedSignature === req.body.razorpay_signature){
-//     console.log("Payment Success");
-//     //sending product details to its owner
-//   }else{
-//     console.log("Payment Fail");
-//     res.redirect("/home");
-//   }
- 
-// })
-
-
-//listing products
-
-
- 
 module.exports = router;
