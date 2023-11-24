0724a986429d3f6f90575dffc9b5fcef5adc9d93
rapidemporium <150315341+rapidemporium@users.noreply.github.com>
2023-11-24 13:35:26 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -140,7 +140,7 @@ axios.post("https://moogold.com/wp-json/v1/api/order/create_order", payload, {
 router.get('/products', async (req, res) => {
   try {
   
-    const category_id = 1; // Replace with the desired category ID
+    const category_id = 50; // Replace with the desired category ID
     const productList = {
       path: "product/list_product",
       category_id: category_id
